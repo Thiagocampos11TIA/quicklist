e2b67202601552ecd0f6a11566a21af912181d1b
Thiago Campos <thiago0yb@gmail.com>
2025-05-31 23:26:32 -0300

    arrumada a parte do item.checked = !item.checked, adição do !

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function showItemsList() {
         sectionList.innerHTML += `
         <div class="item">
                 <div>
-                    <input type="checkbox" name="list" id="item-${index}">
+                    <input type="checkbox" name="list" id="item-${index}"}>
 
                     <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                         <img src="/assets/checked.svg" alt="checked">
@@ -59,7 +59,7 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
-    item.checked = item.checked
+    item.checked = !item.checked
 
     showItemsList()
 }
