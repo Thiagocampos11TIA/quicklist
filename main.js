edf2d90383ad791c9cc112e3815869637930b0f2
Thiago Campos <thiago0yb@gmail.com>
2025-05-31 23:41:58 -0300

    persistencia das informações da lista no localStorage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const items = []
+let items = []
 
 function addItem() {
     const itemName = document.querySelector("#item").value
@@ -42,6 +42,8 @@ function showItemsList() {
             </div>`
 
     })
+
+    localStorage.setItem("items", JSON.stringify(items))
 }
 
 function removeItem(itemName) {
@@ -72,3 +74,14 @@ function checkItem(itemName) {
 function addHideWarningClass() {
     document.querySelector(".warning").classList.add("hide-warning")
 }
+
+function verifyLocalStorageItems() {
+    const localStorageItems = localStorage.getItem("items")
+
+    if (localStorageItems) {
+        items = JSON.parse(localStorageItems)
+        showItemsList()
+    }
+}
+
+verifyLocalStorageItems()
