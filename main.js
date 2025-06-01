dea77cdc6d15bb74580feb2760605c10707c3446
Thiago Campos <thiago0yb@gmail.com>
2025-05-31 23:18:10 -0300

    Ultimo ajuste da função checkItem

--- main.js
+++ main.js
@@ -59,12 +59,7 @@ function removeItem(itemName) {
 
 function checkItem(itemName) {
     const item = items.find((item) => item.name === itemName)
-
-    if(item.checked === true) {
-        item.checked = false
-    } else {
-        item.checked = true
-    }
+    item.checked = item.checked
 
     showItemsList()
 }
